438aeb086ed856d6da17244a4e78510f29094cbd
Ryan Sweet <rysweet@microsoft.com>
2025-09-17 08:28:18 -0700

    fix uninstall

--- .claude/tools/amplihack/uninstall.sh
+++ .claude/tools/amplihack/uninstall.sh
@@ -8,3 +8,4 @@ rm -rf ~/.claude/tools/amplihack
 # copy the ~/.claude/settings.json file to ~/.claude/settings.json.bak.amplihack as a backup
 cp ~/.claude/settings.json ~/.claude/settings.json.bak.amplihack
 echo "Backup of ~/.claude/settings.json created at ~/.claude/settings.json.bak.amplihack"
+rm -rf ~/.claude/settings.json
